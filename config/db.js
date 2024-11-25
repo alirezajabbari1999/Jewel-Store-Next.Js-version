c33b721bb7a1a43152b78c666e0e1aeca93f4ac3
Alireza <alirezammww@gmail.com>
2024-11-25 17:54:46 +0330

    update

--- config/db.js
+++ config/db.js
@@ -5,7 +5,10 @@ export default async function connectToDB() {
     if (mongoose.connections[0].readyState) {
       return false;
     }
-    await mongoose.connect("mongodb://localhost:27017/jewel-store");
+    await mongoose.connect("mongodb://localhost:27017/jewel-store", {
+      connectTimeoutMS: 30000, // تایم‌اوت برای اتصال
+      socketTimeoutMS: 45000, // تایم‌اوت سوکت
+    });
     console.log("Connected to the database successfully");
   } catch (err) {
     console.log("Faild to connect to the DB", err);
